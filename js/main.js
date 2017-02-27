ad58fe911ca30567466c488c5ce1d1f0752328ee
Pachj <Pachj@users.noreply.github.com>
2017-02-27 20:05:27 +0100

    some js "features" added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
 /**
  * Created by Henry on 25.02.17.
  */
+let calculation = {
+    firstNumber: "",
+    secondNumber: "",
+    operator: "",
+    result: 0
+};
+
+let isSecondNumber = false;
+
+$(document).ready(function () {
+    $(":button").click(function () {
+        let newInput = $(this).attr("value");
+        if (isOperator(newInput)) {
+            console.log("Is Operator!")
+        }
+        else {
+            updateNumber(newInput);
+        }
+    })
+});
+
+// checks if input is an operator
+let isOperator = (newInput) => {
+    const operators = ["clear", "signed-integer", "rest", "division", "multiplication", "subtraction", "addition", "equals"];
+    for (let i = 0; i < operators.length; i++) {
+        if (newInput === operators[i]) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// adds the new digit to a number
+function updateNumber(newDigit) {
+    if (!isSecondNumber) {
+        calculation.firstNumber += newDigit;
+    }
+    else {
+        calculation.secondNumber += newDigit;
+    }
+}
