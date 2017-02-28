3cdc8aeee35ea8d146a6240df0427b80de1afefc
Pachj <Pachj@users.noreply.github.com>
2017-02-28 17:25:57 +0100

    can handle clear and division and multiplication and rest

--- js/main.js
+++ js/main.js
@@ -16,8 +16,17 @@ $(document).ready(function () {
 
 function nextStep(input) {
     switch (input) {
+        case "clear":
+            clearCalculator();
+            break;
 
-        default:
+        case "multiplication":
+        case "division":
+        case "rest":
+            calculate(input);
+            break;
+
+        default: // ToDo: max number length
             if (!isSecondNumber) {
                 result += input;
                 showInput(result);
@@ -28,6 +37,41 @@ function nextStep(input) {
             }
             break;
     }
+}
+
+function calculate(newOperator) {
+    if (operator !== "") {
+        switch (operator) {
+            case "rest":
+                result = parseFloat(result % secondNumber);
+                break;
+
+            case "multiplication":
+                result = parseFloat(result * secondNumber);
+                break;
+
+            case "division":
+                result = parseFloat(result / secondNumber);
+                break;
+        }
+        operator = newOperator;
+        showSecond(result); //FixMe: Didn't show result in second-display
+    }
+}
+
+
+function clearCalculator() {
+    result = "";
+    secondNumber = "";
+    operator = "";
+    isSecondNumber = false;
+
+    $("#main-display").html(0);
+    $("#second-display").html();
+}
+
+function showSecond(valueToDisplay) {
+    $("#second-display").html(valueToDisplay);
 }
 
 function showInput(inputToDisplay) {
