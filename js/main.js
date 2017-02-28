ed4c703b27b8aba2242806f52de4624259b8236f
Pachj <Pachj@users.noreply.github.com>
2017-02-28 16:49:52 +0100

    can handle and display number inputs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 /**
  * Created by Henry on 28.02.17.
  */
+let result = "";
+let secondNumber = "";
+let operator = "";
+let isSecondNumber = false;
+
+$(document).ready(function () {
+    $(":button").click(function () {
+        let input = $(this).attr("value");
+
+        nextStep(input);
+    });
+});
+
+function nextStep(input) {
+    switch (input) {
+
+        default:
+            if (!isSecondNumber) {
+                result += input;
+                showInput(result);
+            }
+            else {
+                secondNumber += input;
+                showInput(secondNumber);
+            }
+            break;
+    }
+}
+
+function showInput(inputToDisplay) {
+    $("#main-display").html(inputToDisplay);
+}
